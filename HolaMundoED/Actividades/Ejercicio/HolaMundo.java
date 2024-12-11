866e33046222d119cd20fbaf8a75d1984793fd1a
AroaPons <apons20484@iesjoanramis.org>
2024-12-11 19:39:31 +0100

    Primer commit

---
+++ HolaMundoED/Actividades/Ejercicio/HolaMundo.java
@@ -0,0 +1,18 @@
+package Ejercicio;
+
+import java.util.Scanner;
+
+public class HolaMundo {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Ingresa tu nombre: ");
+		String nombre = scanner.nextLine();
+		System.out.println("¡Hola, " + nombre + "! Bienvenido a Java.");
+	}
+}
+
+// Creación de archivo HolaMundo.java
+// Cambio realizado desde rama desarrollo
+// Comentario después de realizar fusión
